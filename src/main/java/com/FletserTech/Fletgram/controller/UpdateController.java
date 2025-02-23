dbd1514c87969344096c498d113a343e769ae913
FletserW <reidosgames147@hotmail.com>
2025-02-23 18:48:25 -0300

    Sistema de atualização

---
+++ src/main/java/com/FletserTech/Fletgram/controller/UpdateController.java
@@ -0,0 +1,23 @@
+package com.FletserTech.Fletgram.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/update")
+public class UpdateController {
+
+    @GetMapping("/latest")
+    public ResponseEntity<Map<String, String>> getLatestVersion() {
+        Map<String, String> response = new HashMap<>();
+        response.put("version", "1.0.5"); // Número da versão atual
+        response.put("url", "electric-polished-perch.ngrok-free.app/static/apk/fletgram_1.1.0.apk"); // Link do APK
+        return ResponseEntity.ok(response);
+    }
+}
+
